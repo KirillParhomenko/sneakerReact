cb8f6630426ea4965a7195d89f8fe8a1e069ba6b
kparkhomenka <KParkhomenka@gomel.iba.by>
2022-09-23 18:57:30 +0300

    a-little-bit-of-change-in-banner-img

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,4 +1,5 @@
 import BannerTitle from "./BannerTitle";
+import BannerImage from "../Img/BannerImage.png";
 
 import "./Banner.css";
 
@@ -6,6 +7,7 @@ const Banner = () => {
   return (
     <div className="banner">
       <BannerTitle />
+      <img src={BannerImage}/>
     </div>
   );
 };
